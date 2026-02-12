c802382e73a90126b85512734783fbaf93e35cad
Hozen <enzo.durel@gmail.com>
2026-02-12 18:34:51 +0100

    add project components for portfolio

---
+++ src/app/components/project/project.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { ProjectModel } from '../../core/models/project.model';
+import { ImageDepthStack } from '../image-depth-stack/image-depth-stack';
+
+@Component({
+  selector: 'app-project',
+  imports: [ImageDepthStack],
+  templateUrl: './project.html',
+  styleUrl: './project.scss',
+})
+export class Project {
+    project = input.required<ProjectModel>();
+}
