c7059fd054c7561743bf9bde373599259d7a11c7
Hozen <enzo.durel@gmail.com>
2026-02-15 23:09:16 +0100

    correct router link on home link cards

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Timeline } from '../../components/timeline/timeline';
 import { Contact } from '../../components/contact/contact';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
-    imports: [Timeline, Contact],
+    imports: [Timeline, Contact, RouterLink],
     templateUrl: './home.html',
     styleUrl: './home.scss',
 })
