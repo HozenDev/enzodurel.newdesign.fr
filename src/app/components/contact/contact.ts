3d55129a6dcec6229355a219b47215efd841b444
Hozen <enzo.durel@gmail.com>
2026-02-25 18:26:01 +0100

    add email integration and better form ui

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -11,23 +12,39 @@ import { CommonModule } from '@angular/common';
 })
 export class Contact {
     contactForm: FormGroup;
+    isSending = false;
 
     constructor(private fb: FormBuilder) {
-	this.contactForm = this.fb.group({
-	    name: ['', Validators.required],
-	    email: ['', [Validators.required, Validators.email]],
-	    subject: ['', Validators.required],
-	    message: ['', Validators.required]
-	});
+        this.contactForm = this.fb.group({
+            name: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            subject: ['', Validators.required],
+            message: ['', Validators.required]
+        });
     }
 
-    onSubmit(): void {
-	if (this.contactForm.valid) {
-	    console.log('Form submitted:', this.contactForm.value);
-	    // TODO: connect to API / email service
-	    this.contactForm.reset();
-	} else {
-	    this.contactForm.markAllAsTouched();
-	}
+    onSubmit(formNativeElement: HTMLFormElement): void {
+        if (this.contactForm.valid) {
+            this.isSending = true; // On commence l'envoi
+
+            emailjs.sendForm(
+                'service_ykk8vgt',
+                'template_gz51cpt',
+                formNativeElement,
+                'pT7y-bNDvgWdfcfqM')
+                .then(() => {
+                    alert('Mail envoyé avec succès!');
+                    this.contactForm.reset();
+                })
+                .catch((error) => {
+                    console.error('Erreur :', error);
+                    alert('Une erreur est survenue.');
+                })
+                .finally(() => {
+                    this.isSending = false; // On libère le bouton
+                });
+        } else {
+            this.contactForm.markAllAsTouched();
+        }
     }
 }
